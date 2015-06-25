0b0464eb85099e449f8e2b9f7c48f2fa18d362f0
Matthew Gould <gouldm44@gmail.com>
2015-06-25 10:16:14 -0400

    gets to the search results page but doesnt navigate to the cardinal page

--- challenge_1.rb
+++ challenge_1.rb
@@ -4,17 +4,19 @@ require 'pry'
 def firefox_browse
   # opens new instance of google in Firefox
   browser = Watir::Browser.new :firefox
-  browser.goto 'https://www.google.com/'
+  browser.goto 'http://google.com'
 
   # enters text into search field and clicks submit button
   browser.text_field(:id => 'lst-ib').set 'cardinal solutions'
-  browser.button(:name => 'btnK').click
+  browser.button(:name => 'btnG').click
 
   # selects the first link (cardinal solutions homepage in this case)
   browser.h3s(:class => 'r')[0].click
 
   # navigates to the 'careers' section of CS page and puts the necessary text
   browser.link(:href => '/careers').click
-  answer = browser.div(:class => 'hp-slideshow-caption-title').p.text
+  answer = browser.div(:class => 'hp-slideshow-caption-title').p[0].text
   puts answer
 end
+
+firefox_browse
