1f9dff9fbeaae0e516df1d6d6b5e12fee977c5e2
Matthew Gould <gouldm44@gmail.com>
2015-06-25 09:59:47 -0400

    changed method name and added comments for each section

--- challenge_1.rb
+++ challenge_1.rb
@@ -1,10 +1,15 @@
 require 'watir-webdriver'
 require 'pry'
 
-def browse
+def firefox_browse
+  # opens new instance of google in Firefox
   browser = Watir::Browser.new :firefox
   browser.goto 'https://www.google.com/'
 
+  # enters text into search field and clicks submit button
   browser.text_field(:id => 'lst-ib').set 'cardinal solutions'
   browser.button(:name => 'btnK').click
+
+  # selects the first link (cardinal solutions homepage in this case)
+  browser.h3s(:class=>'r')[0].click 
 end
