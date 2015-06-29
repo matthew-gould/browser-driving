e2bfbc812793eeb4f73da3c802e580c1439d65b5
Matthew Gould <gouldm44@gmail.com>
2015-06-29 09:07:12 -0400

    You can choose browser type by typing chome or firefox after the filename on the command line

--- challenge_3.rb
+++ challenge_3.rb
@@ -2,7 +2,32 @@ require 'watir-webdriver'
 require 'pry'
 
 def browse
+  # takes the browser type (chrome or firefox) on the command line
+  if ARGV[0] == "chrome"
+    arg = :chrome
+  elsif ARGV[0] == "firefox"
+    arg = :firefox
+  else
+    puts "You didn't make a valid choice"
+    return
+  end
 
+  # opens new instance of google in Firefox
+  browser = Watir::Browser.new(arg)
+  browser.goto 'http://google.com'
+
+  # enters text into search field and clicks submit button
+  browser.text_field(:id => 'lst-ib').set 'cardinal solutions'
+  browser.button(:name => 'btnG').click
+
+  # selects the first link (cardinal solutions homepage in this case) after page loads
+  Watir::Wait.until { browser.text.include? 'cardinal' }
+  browser.h3(:class => 'r').link.click
+
+  # navigates to the 'careers' section of CS page and puts the necessary text
+  browser.link(:href => '/careers').click
+  answer = browser.div(:class => 'textTitleImageWrap image-avail').h1.text
+  puts answer
 end
 
 browse
