09db9d90c9eca0a26fe34ed92bb3184c02d8afd4
Matthew Gould <gouldm44@gmail.com>
2015-07-01 11:44:39 -0400

    didn't use this class yet

--- page-objects/cardinalhome.rb
+++ page-objects/cardinalhome.rb
@@ -7,12 +7,4 @@ class CardinalHome < SearchPage
   include PageObject
 
   link(:careers, :href => '/careers')
-
-  def initialize(browser)
-    super(browser)
-  end
-
-  def navigate_to_careers
-    @@browser.careers
-  end
 end
