7657b27e33e7f34353064bd2ae5a10f353f1e5a8
Matthew Gould <gouldm44@gmail.com>
2015-07-02 12:05:20 -0400

    making changed for page object design

--- challenge_5.rb
+++ challenge_5.rb
@@ -1,21 +1,23 @@
 require 'watir-webdriver'
 require 'page-object'
 require 'pry'
+require './page-objects/searchpage.rb'
+require './page-objects/cardinalhome.rb'
 
-class GoogleSearch
-  include PageObject
+def browse
 
-  browser = Watir::Browser.new :chrome
-  # browser = Watir::Browser.new(ENV['BROWSER_TYPE'].to_sym)
-  browser.window.resize_to('1200', '900')
-  browser.goto 'http://google.com'
+    browser_type = ENV['BROWSER_TYPE'].to_sym || :chrome
+    browser_size = ENV['BROWSER_SIZE'] || '1000'
 
-  text_field(:search, :id => 'lst-ib')
-  button(:submit, :value => 'Search')
-
-  google_search = GoogleSearch.new(browser)
-  google_search.search= 'cardinal solutions'
-  google_search.submit
+    # opens new instance of google
+    browser = Watir::Browser.new(browser_type)
+    browser.window.resize_to(browser_size, '850')
+    browser.goto 'http://google.com'
+    return browser
 end
 
-
+my_page_object = SearchPage.new(browse)
+my_page_object.search('cardinal solutions')
+my_page_object.navigate_to_link
+my_page_object.go_to_careers
+my_page_object.get_text
