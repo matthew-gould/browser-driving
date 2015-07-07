dea5a6027e136418d913b5c065c2035521a5062c
Matthew Gould <gouldm44@gmail.com>
2015-07-07 15:28:57 -0400

    trying to make this work and added instance variables to the steps

---
+++ calculator/lib/calculator.rb
@@ -0,0 +1,19 @@
+module Calculator
+
+  def self.add(*numbers)
+    numbers.inject(:+)
+  end
+
+  def self.subtract(*numbers)
+    numbers.inject(:-)
+  end
+
+  def self.multiply(*numbers)
+    numbers.reduce(:*)
+  end
+
+  def self.divide(*numbers)
+    numbers.reduce(:/)
+  end
+
+end
