e6df5f1fdd40db3ab51433c415559bdb573a5710
Matthew Gould <gouldm44@gmail.com>
2015-07-08 13:22:53 -0400

    fixed syntax in addition file

--- calculator/features/step_definitions/addition_steps.rb
+++ calculator/features/step_definitions/addition_steps.rb
@@ -1,37 +1,37 @@
 When /^I am adding (\d+) and (\d+)$/ do |num1, num2|
-  @sum = Calculator.add(num1, num2)
+  @sum = Calculator.add(num1.to_i, num2.to_i)
 end
 
 Then /^I should get back a (\d+)$/ do |num3|
-  expect(@sum == num3)
+  expect(@sum).to eq num3.to_i
 end
 
 When /^I am adding zero (\d+) and (\d+)$/ do |num1, num2|
-  @sum = Calculator.add(num1, num2)
+  @sum = Calculator.add(num1.to_i, num2.to_i)
 end
     
 Then /^I should get back (\d+)$/ do |num3|
-  expect(@sum == num3)
+  expect(@sum).to eq num3.to_i
 end
 
 When /^I add (\d+) and (\d+)$/ do |num1, num2|
-  @sum = Calculator.add(num1, num2)
-  @sum2 = Calculator.add(num2, num1)
+  @sum = Calculator.add(num1.to_i, num2.to_i)
+  @sum2 = Calculator.add(num2.to_i, num1.to_i)
 end
 
 Then /^I should get back (\d+) regardless of how they are ordered$/ do |num3|
-  expect(@sum == num3)
-  expect(@sum2 == num3)
+  expect(@sum).to eq num3.to_i
+  expect(@sum2).to eq num3.to_i
 end
 
 When /^I add (\d+), (\d+) and (\d+) together$/ do |num1, num2, num3|
-  @sum = Calculator.add(num1, num2, num3)
-  @sum2 = Calculator.add(num2, num3, num1)
-  @sum3 = Calculator.add(num3, num1, num2)
+  @sum = Calculator.add(num1.to_i, num2.to_i, num3.to_i)
+  @sum2 = Calculator.add(num2.to_i, num3.to_i, num1.to_i)
+  @sum3 = Calculator.add(num3.to_i, num1.to_i, num2.to_i)
 end
 
 Then /^I should get (\d+) regardless of how they are ordered$/ do |num4|
-  expect(@sum == num4)
-  expect(@sum2 == num4)
-  expect(@sum3 == num4)
+  expect(@sum).to eq num4.to_i
+  expect(@sum2).to eq num4.to_i
+  expect(@sum3).to eq num4.to_i
 end
